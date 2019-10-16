f2c3de6ab51a32b81183a342aad56462510a31a5
Brandon Thayer <blthayer@tamu.edu>
2019-10-15 19:56:40 -0500

    Updating README and setup.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gym-powerworld
-Environment for using PowerWorld Simulator with OpenAI Gym
+Environment for using PowerWorld Simulator with OpenAI Gym.
+
+## Directories
+### gym_powerworld
+Python package which conforms to OpenAI Gym's layout.
+
+### tests
+Tests associated with gym_powerworld.
+
+## Git LFS
+This repository leverages Git Large File Storage (LFS) to handle large
+files (such as Powerworld binaries). Simply install git-lfs from their
+[website](https://git-lfs.github.com/), open Git Bash, change directories
+to this repository, and run `git lfs install`.
+
--- setup.py
+++ setup.py
@@ -1,7 +1,40 @@
+"""
+Reference:
+https://github.com/pypa/sampleproject/blob/master/setup.py
+"""
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+
+    long_description = f.read()
 
 setup(name='gym_powerworld',
       version='0.0.1',
+      description=('OpenAI gym environment for interfacing with PowerWorld '
+                   'Simulator'),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/blthayer/gym-powerworld',
+      author='Brandon Thayer',
+      author_email='blthayer@tamu.edu',
+      # For a list of valid classifiers, see
+      # https://pypi.org/classifiers/
+      classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 3.6'
+            # TODO: Add 3.7 after figuring out pywin32 issues
+      ],
+      keywords=('deep reinforcement learning machine PowerWorld smart grid '
+                'voltage control electric power system'),
+      # TODO: Add 3.7 after figuring out pywin32 issues
+      python_requires='==3.6.*',
       install_requires=[
             'gym',
             'numpy',
